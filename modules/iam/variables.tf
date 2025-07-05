6e79f27ef2e4064d1d9fd387598ffee3f57dad7b
Ldrex1 <damzealwini7@gmail.com>
2025-07-05 21:18:55 +0100

    update: minor variable additons and format

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,23 +1,24 @@
-variable "name" {
+variable "role_name" {
   description = "role name"
-  type = string
+  type        = string
+  default     = "iam-role"
 }
 
 variable "assume_role_policy" {
-    description = "Iam role assume policy in object form"
+  description = "Iam role assume policy in object form"
   type = object({
     Version = optional(string, "2012-10-17")
     Statement = list(object({
-        Effect = string
-        Principal = map(any)
-        Action=list(string)
-        Conditon = optional(map(any))
-    })) 
+      Effect    = string
+      Principal = map(any)
+      Action    = list(string)
+      Conditon  = optional(map(any))
+    }))
   })
 }
 
 variable "role_tags" {
-  type = map(any)
+  type        = map(any)
   description = "role tags"
   default = {
   }
